3cf346e6c852f10cabe7c9f8c0d4ca6a10ca358c
jlillioja <jlillioja@gmail.com>
2018-08-28 16:59:41 -0500

    Issue push notifications when app in foreground

--- app/src/main/java/io/grandlabs/ift/messaging/IftFirebaseMessagingService.kt
+++ app/src/main/java/io/grandlabs/ift/messaging/IftFirebaseMessagingService.kt
@@ -1,7 +1,16 @@
 package io.grandlabs.ift.messaging
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.media.RingtoneManager
+import android.os.Build
+import android.support.v4.app.NotificationCompat
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import io.grandlabs.ift.IftApp
+import io.grandlabs.ift.R
 import io.grandlabs.ift.login.SessionManager
 import javax.inject.Inject
 
@@ -17,6 +26,8 @@ class IftFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     private val LOG_TAG = this::class.simpleName
+    private val iftChannelId = "IftChannel"
+    private val iftchannelName = "IFT Push Notifications"
 
     /**
      * Called if InstanceID token is updated. This may occur if the security of
@@ -29,6 +40,40 @@ class IftFirebaseMessagingService : FirebaseMessagingService() {
         // Instance ID token to your app server.
 
         sessionManager.updateDeviceToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage?) {
+        super.onMessageReceived(message)
+
+        message?.notification?.send()
+
+        Log.d(LOG_TAG, "Message received: ${message?.messageId}")
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private fun RemoteMessage.Notification.send() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationBuilder = NotificationCompat.Builder(this@IftFirebaseMessagingService, iftChannelId)
+                .setSmallIcon(R.drawable.app_icon_small)
+                .setContentTitle(this.title)
+                .setContentText(this.body)
+                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(iftChannelId,
+                    iftchannelName,
+                    NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
     }
 
 }
